bf2cb7364100ed38050b82a3c578b38eb197e751
Aleksandr Yershov <aleksandr.yershov@dysnix.com>
2021-10-22 15:17:39 +0300

    add helper for parse prometheus metrics timestamp

---
+++ external/types_convertation/time.go
@@ -0,0 +1,41 @@
+package types_convertation
+
+import (
+	"strconv"
+	"time"
+)
+
+func ParseMillisecondUnixTimestamp(s interface{}) (res time.Time, err error) {
+	var ts int64
+	switch tmp := s.(type) {
+	case string:
+		ts, err = strconv.ParseInt(tmp, 10, 64)
+		if err != nil {
+			return time.Time{}, err
+		}
+	case uint64:
+		ts = int64(tmp)
+	case int8:
+		ts = int64(tmp)
+	case int16:
+		ts = int64(tmp)
+	case int32:
+		ts = int64(tmp)
+	case int:
+		ts = int64(tmp)
+	case int64:
+		ts = tmp
+	case uint:
+		ts = int64(tmp)
+	case uint8:
+		ts = int64(tmp)
+	case uint16:
+		ts = int64(tmp)
+	case uint32:
+	}
+
+	sec := ts / 1000
+	msec := ts % 1000
+
+	return time.Unix(sec, msec*int64(time.Millisecond)), nil
+}
