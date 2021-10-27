701efe0ea860b616ddb65d4dea66ab201b8af896
Aleksandr Yershov <aleksandr.yershov@dysnix.com>
2021-10-27 19:26:07 +0300

    add helpers for work with context.

--- external/http_transport/context_helpers.go
+++ external/http_transport/context_helpers.go
@@ -8,7 +8,7 @@ import (
 
 type keyCtx string
 
-func addToContext(ctx context.Context, key string, data interface{}) context.Context {
+func AddToContext(ctx context.Context, key string, data interface{}) context.Context {
 	if req, ok := ctx.(*fasthttp.RequestCtx); ok {
 		req.SetUserValue(key, data)
 		return req
@@ -17,7 +17,7 @@ func addToContext(ctx context.Context, key string, data interface{}) context.Con
 	return context.WithValue(ctx, keyCtx(key), data)
 }
 
-func getFromContext(ctx context.Context, key string) interface{} {
+func GetFromContext(ctx context.Context, key string) interface{} {
 	if req, ok := ctx.(*fasthttp.RequestCtx); ok {
 		return req.UserValue(key)
 	}
